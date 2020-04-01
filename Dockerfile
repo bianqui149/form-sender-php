f55a6f4fd2b499e2d90b473dadb549f569d05ab5
Julian Bianqui <bianquijulian@gmail.com>
2020-04-01 11:38:59 -0300

    adding mpdf

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 # Base image
 FROM php:7.2-apache
 COPY ./src /var/www/html/
+COPY --chown=www-data:www-data ./src/ /var/www/html/
 EXPOSE 80
--- src/index.php
+++ src/index.php
@@ -1,1 +1,6 @@
-<?php echo "hola mundo";?>
+<?php 
+require_once __DIR__ . '/vendor/autoload.php';
+
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1>Hello world!</h1>');
+$mpdf->Output();
